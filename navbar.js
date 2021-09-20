f043ff4945c4826b9e7b44eba43aa60a46f0c07c
Jean Carlo San Juan <jcsanjuan12@gmail.com>
2021-09-20 17:35:24 +0800

    navbar done

---
+++ navbar.js
@@ -0,0 +1,12 @@
+const nav = document.getElementById("navbar");
+
+const pages = {
+    "Home":"index.html",
+    "About": "about.html"
+};
+
+// Adds nav bar links based on the dictionary entries of pages
+Object.keys(pages).forEach(name => {
+    const href= pages[name];
+    nav.innerHTML += `<a href=${href}>${name}</a>`;
+})
