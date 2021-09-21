7da6f042c11a33c49b555266136dcaecf089d562
Jean Carlo San Juan <jcsanjuan12@gmail.com>
2021-09-21 14:22:19 +0800

    changed link order

--- navbar.js
+++ navbar.js
@@ -2,9 +2,9 @@ const nav = document.getElementById("navbar");
 
 const pages = {
     "Home":"index.html",
-    "About": "about.html",
     "Ice Cream Flavors": "flavors.html",
-    "Order Online": "order.html"
+    "Order Online": "order.html",
+    "About": "about.html",
 };
 
 // Adds nav bar links based on the dictionary entries of pages
