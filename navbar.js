dbf759f157085f9d818fdc95eec55d71aa868e9f
Jean Carlo San Juan <jcsanjuan12@gmail.com>
2021-09-21 17:17:18 +0800

    made css files

--- navbar.js
+++ navbar.js
@@ -12,7 +12,7 @@ Object.keys(pages).forEach(name => {
     const href= pages[name];
     const title = document.title.toLocaleLowerCase()
     const _name = name.toLocaleLowerCase();
-    const current = (title.search(_name) != -1) ? "id=current":"";
+    const is_current = (title.search(_name) != -1) ? "id=current":"";
 
-    nav.innerHTML += `<a href=${href} ${current}>${name}</a>`;
+    nav.innerHTML += `<a href=${href} ${is_current}>${name}</a>`;
 })
