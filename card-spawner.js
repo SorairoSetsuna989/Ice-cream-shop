8d70626d145cb29971c584251f46bc00abcf08f3
Jean Carlo San Juan <jcsanjuan12@gmail.com>
2021-09-24 09:42:27 +0800

    adjusted some comments

--- card-spawner.js
+++ card-spawner.js
@@ -1,11 +1,20 @@
+// Script usage:
+// In the html document's head tag in which you have linked this file, create a script tag
+// inside it declare a javascript object called "obj"
+// Right before the colon, type the text that you want to see in the card-info div class.
+// After the colon, Put the image's url that you want to see as the background for the div that has a class: card-image.
+// see desserts.html to see it in action
+
+//Example: 
+// var obj = {
+//  "text" : "path or url of the image"
+// }
+
+
+
 const main = document.getElementById("main");
 
-// How to use the function below:
-  // In the html document's head tag create a script tag
-  // inside it create an object called obj
-  // Put the text that you want to see in the div that has a class: card-info right before the colon.
-  // Put the image's url that you want to see as the background for the div that has a class: card-image after the colon.
-  // see desserts.html as an example
+
 function spawn_cards(obj) {
   Object.keys(obj).forEach(key => {
     main.innerHTML += `
